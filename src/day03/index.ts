78aeeb65e460fca4dcaac23a7f4979f8cecb792a
Jason Paris <paris3200@gmail.com>
2025-12-04 08:46:25 -0500

    refactor: refactor day 3 and lint

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -1,98 +1,98 @@
 import run from "aocrunner";
 
 const parseInput = (rawInput: string) => {
-  const input = rawInput.split("\n");
-  return input;
+	const input = rawInput.split("\n");
+	return input;
 };
 
 const buildLargestNumber = (bat: number[], count: number): number => {
-  const range: number[] = [];
-  const result: number[] = [];
-  for (let i = count - 1; i >= 0; i--) {
-    range.push(i);
-    result.push(0);
-  }
+	const range: number[] = [];
+	const result: number[] = [];
+	for (let i = count - 1; i >= 0; i--) {
+		range.push(i);
+		result.push(0);
+	}
 
-  let resultIndex = 0;
+	let resultIndex = 0;
 
-  let index = 0;
-  for (const offset of range) {
-    for (let i = index; i < bat.length - offset; i++) {
-      if (bat[i] > result[resultIndex]) {
-        result[resultIndex] = bat[i];
-        index = i;
-      }
-    }
-    index++;
-    resultIndex++;
-  }
+	let index = 0;
+	for (const offset of range) {
+		for (let i = index; i < bat.length - offset; i++) {
+			if (bat[i] > result[resultIndex]) {
+				result[resultIndex] = bat[i];
+				index = i;
+			}
+		}
+		index++;
+		resultIndex++;
+	}
 
-  return Number(result.join(""));
+	return Number(result.join(""));
 };
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+	const input = parseInput(rawInput);
 
-  const jolts = [];
-  for (const bank of input) {
-    const batt = bank.split("").map(Number);
-    jolts.push(buildLargestNumber(batt, 2));
-  }
+	const jolts = [];
+	for (const bank of input) {
+		const batt = bank.split("").map(Number);
+		jolts.push(buildLargestNumber(batt, 2));
+	}
 
-  let sum = 0;
-  jolts.forEach((number) => {
-    sum += number;
-  });
+	let sum = 0;
+	jolts.forEach((number) => {
+		sum += number;
+	});
 
-  return sum.toString();
+	return sum.toString();
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+	const input = parseInput(rawInput);
 
-  const jolts = [];
-  for (const bank of input) {
-    const batt = bank.split("").map(Number);
-    jolts.push(buildLargestNumber(batt, 12));
-  }
+	const jolts = [];
+	for (const bank of input) {
+		const batt = bank.split("").map(Number);
+		jolts.push(buildLargestNumber(batt, 12));
+	}
 
-  let sum = 0;
-  jolts.forEach((number) => {
-    sum += number;
-  });
+	let sum = 0;
+	jolts.forEach((number) => {
+		sum += number;
+	});
 
-  return sum.toString();
+	return sum.toString();
 };
 
 run({
-  part1: {
-    tests: [
-      {
-        input: `
+	part1: {
+		tests: [
+			{
+				input: `
           987654321111111
           811111111111119
           234234234234278
           818181911112111
 `,
-        expected: "357",
-      },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-      {
-        input: `
+				expected: "357",
+			},
+		],
+		solution: part1,
+	},
+	part2: {
+		tests: [
+			{
+				input: `
           987654321111111
           811111111111119
           234234234234278
           818181911112111
 `,
-        expected: "3121910778619",
-      },
-    ],
-    solution: part2,
-  },
-  trimTestInputs: true,
-  onlyTests: true,
+				expected: "3121910778619",
+			},
+		],
+		solution: part2,
+	},
+	trimTestInputs: true,
+	onlyTests: true,
 });
--- src/day04/index.ts
+++ src/day04/index.ts
@@ -1,120 +1,99 @@
 import run from "aocrunner";
-import { dir } from "console";
 
 const parseInput = (rawInput: string) => {
-  const raw_input = rawInput.split("\n");
-  let input = []
-  for (const row of raw_input) {
-    input.push(row.split(''))
-  }
-  return input;
+	return rawInput.split("\n").map((row) => row.split(""));
 };
 
-const getNeighbors = (grid: string[][], point: number[], directions: number[][]) => {
+const getNeighbors = (
+	grid: string[][],
+	point: number[],
+	directions: number[][],
+) => {
+	let neighbors = [];
 
-  let points = directions.length
-  let neighbors = []
+	for (let i = 0; i < directions.length; i++) {
+		let targetRow = directions[i][0] + point[0];
+		let targetCol = directions[i][1] + point[1];
 
+		if (
+			0 <= targetRow &&
+			targetRow < grid.length &&
+			0 <= targetCol &&
+			targetCol < grid[0].length
+		) {
+			neighbors.push(grid[targetRow][targetCol]);
+		}
+	}
 
-  for (let i = 0; i < points; i++) {
-    let targetRow = directions[i][0] + point[0];
-    let targetCol = directions[i][1] + point[1];
-
-    if (0 <= targetRow && targetRow < grid.length && 0 <= targetCol && targetCol < grid[0].length) {
-      neighbors.push(grid[targetRow][targetCol])
-    }
-  }
-
-  return neighbors
-}
+	return neighbors;
+};
 
 const processGrid = (grid: string[][]): [string[][], number] => {
+	const directions = [
+		[-1, -1],
+		[-1, 0],
+		[-1, 1],
+		[0, -1],
+		[0, 1],
+		[1, -1],
+		[1, 0],
+		[1, 1],
+	];
 
-  const directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+	let rows = grid.length;
+	let cols = grid[0].length;
+	let count = 0;
+	let removed = [];
 
-  let rows = grid.length
-  let cols = grid[0].length
-  let count = 0;
-  let removed = []
+	for (let i = 0; i < rows; i++) {
+		for (let j = 0; j < cols; j++) {
+			if (grid[i][j] === "@") {
+				let neighbors = getNeighbors(grid, [i, j], directions);
+				let rolls = 0;
+				for (let point of neighbors) {
+					if (point === "@") {
+						rolls++;
+					}
+				}
+				if (rolls < 4) {
+					count++;
+					removed.push([i, j]);
+				}
+			}
+		}
+	}
+	for (let point of removed) {
+		grid[point[0]][point[1]] = ".";
+	}
 
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "@") {
-        let neighbors = getNeighbors(grid, [i, j], directions)
-        let rolls = 0
-        for (let point of neighbors) {
-          if (point === "@") {
-            rolls++
-          }
-        }
-        if (rolls < 4) {
-          count++
-          removed.push([i, j])
-        }
-      }
-    }
-
-  }
-  for (let point of removed) {
-    grid[point[0]][point[1]] = "."
-
-  }
-
-  return [grid, count]
-}
+	return [grid, count];
+};
 
 const part1 = (rawInput: string) => {
-  const grid = parseInput(rawInput);
+	const inputGrid = parseInput(rawInput);
 
-  const directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
-
-  let rows = grid.length
-  let cols = grid[0].length
-  let count = 0;
-
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (grid[i][j] === "@") {
-        let neighbors = getNeighbors(grid, [i, j], directions)
-        let rolls = 0
-        for (let point of neighbors) {
-          if (point === "@") {
-            rolls++
-          }
-        }
-        if (rolls < 4) {
-          count++
-        }
-      }
-
-    }
-  }
-
-  return count.toString();
+	const [_grid, count] = processGrid(inputGrid);
+	return count.toString();
 };
 
 const part2 = (rawInput: string) => {
-  let grid = parseInput(rawInput);
-  let sum = 0;
-  let stop = false
-  while (!stop) {
-    let result = processGrid(grid)
-    if (result[1] != 0) {
-      sum += result[1]
-    } else {
-      stop = true
-    }
-    grid = result[0]
-  }
+	let grid = parseInput(rawInput);
+	let sum = 0;
+	let count;
 
-  return sum.toString();
+	do {
+		[grid, count] = processGrid(grid);
+		sum += count;
+	} while (count !== 0);
+
+	return sum.toString();
 };
 
 run({
-  part1: {
-    tests: [
-      {
-        input: `
+	part1: {
+		tests: [
+			{
+				input: `
         ..@@.@@@@.
         @@@.@.@.@@
         @@@@@.@.@@
@@ -126,15 +105,15 @@ run({
         .@@@@@@@@.
         @.@.@@@.@.
 `,
-        expected: "13",
-      },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-      {
-        input: `
+				expected: "13",
+			},
+		],
+		solution: part1,
+	},
+	part2: {
+		tests: [
+			{
+				input: `
         ..@@.@@@@.
         @@@.@.@.@@
         @@@@@.@.@@
@@ -147,11 +126,11 @@ run({
         @.@.@@@.@.
 
 `,
-        expected: "43",
-      },
-    ],
-    solution: part2,
-  },
-  trimTestInputs: true,
-  onlyTests: false,
+				expected: "43",
+			},
+		],
+		solution: part2,
+	},
+	trimTestInputs: true,
+	onlyTests: false,
 });
